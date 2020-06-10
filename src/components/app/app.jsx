95e7bb7054876b4a7a87c7a84069580dbdf05165
Yuriy <yourgen.edu@mail.ru>
2020-06-10 17:04:51 +0300

    добавил пропсы

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Main from "../main/main.jsx";
 
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {rentOffers} = props;
 
-const App = () => {
   return (
-    <Main />
+    <Main rentOffers={rentOffers}/>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+const Settings = {
+  RENT_OFFERS: 100500
+};
+
 ReactDOM.render(
-    <App />,
+    <App
+      rentOffers={Settings.RENT_OFFERS}
+    />,
     document.querySelector(`#root`)
 );
