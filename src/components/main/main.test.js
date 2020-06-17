ab34f96b7c58289730f9677886ce87590669c088
Yuriy <yourgen.edu@mail.ru>
2020-06-17 14:08:21 +0300

    добавлены снепшот тесты

---
+++ src/components/main/main.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main.jsx";
+
+const TestData = {
+  RENT_OFFERS: [
+    {
+      id: `123`,
+      title: `Beautiful & luxurious apartment at great location`,
+    }, {
+      id: `456`,
+      title: `Wood and stone place`,
+    }, {
+      id: `789`,
+      title: `Canal View Prinsengracht`,
+    }, {
+      id: `012`,
+      title: `Nice, cozy, warm big bed apartment`,
+    }
+  ]
+};
+
+it(`Render Main`, () => {
+  const tree = renderer
+    .create(<Main
+      rentOffers={TestData.RENT_OFFERS}
+      rentOffersCount={TestData.RENT_OFFERS.length}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/components/place-card/place-card.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import PlaceCard from "./place-card.jsx";
+
+const TEST_DATA_TITLE = `Lorem ipsum dolor sit`;
+
+it(`Render PlaceCard`, () => {
+  const tree = renderer
+    .create(<PlaceCard
+      rentOfferTitle={TEST_DATA_TITLE}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
