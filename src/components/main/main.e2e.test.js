f2519a66ebb3e019240982455fed3f5599663825
Yuriy <yourgen.edu@mail.ru>
2020-06-17 16:24:43 +0300

    е2е тест основного компонента

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,32 +1,46 @@
-// import React from "react";
-// import renderer from "react-test-renderer";
-// import Main from "./main.jsx";
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Main from "./main.jsx";
 
-// const TestData = {
-//   RENT_OFFERS: [
-//     {
-//       id: `123`,
-//       title: `Beautiful & luxurious apartment at great location`,
-//     }, {
-//       id: `456`,
-//       title: `Wood and stone place`,
-//     }, {
-//       id: `789`,
-//       title: `Canal View Prinsengracht`,
-//     }, {
-//       id: `012`,
-//       title: `Nice, cozy, warm big bed apartment`,
-//     }
-//   ]
-// };
+const TestData = {
+  RENT_OFFERS: [
+    {
+      id: `123`,
+      title: `Beautiful & luxurious apartment at great location`,
+    }, {
+      id: `456`,
+      title: `Wood and stone place`,
+    }, {
+      id: `789`,
+      title: `Canal View Prinsengracht`,
+    }, {
+      id: `012`,
+      title: `Nice, cozy, warm big bed apartment`,
+    }
+  ]
+};
 
-// it(`Render Main`, () => {
-//   const tree = renderer
-//     .create(<Main
-//       rentOffers={TestData.RENT_OFFERS}
-//       rentOffersCount={TestData.RENT_OFFERS.length}
-//     />)
-//     .toJSON();
+Enzyme.configure({
+  adapter: new Adapter(),
+});
 
-//   expect(tree).toMatchSnapshot();
-// });
+it(`Should header button be pressed`, () => {
+  const onCardTitleClick = jest.fn();
+
+  const main = shallow(
+      <Main
+        rentOffers={TestData.RENT_OFFERS}
+        rentOffersCount={TestData.RENT_OFFERS.length}
+        onCardTitleClick={onCardTitleClick}
+      />
+  );
+
+  const cardTitles = main.find(`.place-card__name a`);
+
+  cardTitles.forEach((title) => {
+    title.props().onClick();
+  });
+
+  expect(onCardTitleClick.mock.calls.length).toBe(cardTitles.length);
+});
