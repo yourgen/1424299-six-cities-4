1a8a14e8edded710aece658a7c3617ef7d068a7a
Yuriy <yourgen.edu@mail.ru>
2020-06-19 14:09:05 +0300

    добавил моковые данные

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
-
-const MockData = {
-  RENT_OFFERS: [
-    {
-      id: Math.random().toString(),
-      title: `Beautiful & luxurious apartment at great location`,
-    }, {
-      id: Math.random().toString(),
-      title: `Wood and stone place`,
-    }, {
-      id: Math.random().toString(),
-      title: `Canal View Prinsengracht`,
-    }, {
-      id: Math.random().toString(),
-      title: `Nice, cozy, warm big bed apartment`,
-    }
-  ]
-};
+import offers from "./mocks/offers.js";
 
 ReactDOM.render(
     <App
-      rentOffers={MockData.RENT_OFFERS}
-      rentOffersCount={MockData.RENT_OFFERS.length}
+      rentOffers={offers}
+      rentOffersCount={offers.length}
     />,
     document.querySelector(`#root`)
 );
---
+++ src/mocks/offers.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    id: Math.random().toString(),
+    title: `Beautiful & luxurious apartment at great location`,
+    img: {
+      src: `img/apartment-01.jpg`,
+      alt: `Place image`
+    },
+    price: `450`,
+    type: `hotel`,
+    rating: 70,
+    isPremium: true,
+  }, {
+    id: Math.random().toString(),
+    title: `Wood and stone place`,
+    img: {
+      src: `img/apartment-02.jpg`,
+      alt: `Place image`
+    },
+    price: `120`,
+    type: `room`,
+    rating: 60,
+    isPremium: false,
+  }, {
+    id: Math.random().toString(),
+    title: `Canal View Prinsengracht`,
+    img: {
+      src: `img/apartment-03.jpg`,
+      alt: `Place image`
+    },
+    price: `570`,
+    type: `house`,
+    rating: 90,
+    isPremium: true,
+  }, {
+    id: Math.random().toString(),
+    title: `Nice, cozy, warm big bed apartment`,
+    img: {
+      src: `img/apartment-04.jpg`,
+      alt: `Place image`
+    },
+    price: `150`,
+    type: `apartment`,
+    rating: 80,
+    isPremium: false,
+  }
+];
